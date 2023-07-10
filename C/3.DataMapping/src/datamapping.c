607de2a2625fae3f9ee414c88fc7e69fedf589ef
Ludovic Capelli <capelli.ludovic@gmail.com>
2023-07-10 05:39:56 +0100

    Doxygen documentation added to data mapping exercise.

--- C/3.DataMapping/src/datamapping.c
+++ C/3.DataMapping/src/datamapping.c
@@ -1,3 +1,19 @@
+/**
+ * @file datamapping.c
+ * @brief This example is to illustrate the different mappings of data.
+ * @details You will find two folders in this example, and will in all examples
+ * provided in this session:
+ * - src: contains the source code.
+ * - bin: contains the binary produced.
+ *
+ * The makefile provided already sets everything up for you:
+ * - To compile: `make`.
+ * - To execute: `./bin/datamapping`.
+ *
+ * If you have any questions, do not hesitate.
+ * @author Ludovic Capelli (l.capelli@epcc.ed.ac.uk)
+ **/
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
